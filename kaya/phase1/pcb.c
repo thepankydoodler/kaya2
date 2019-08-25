15bcf77758d055abaf80c972558456dee61ac8e3
cooperc5 <cooperchris309@gmail.com>
2019-08-25 18:50:00 -0400

    initial commit

---
+++ kaya/phase1/pcb.c
@@ -0,0 +1,84 @@
+
+
+#include "../h/const.h"
+#include "../h/types.h"
+
+#include "/usr/include/uarm/libuarm.h"
+#include "../e/pcb.e"
+
+
+
+HIDDEN pcb_PTR pcbFree_h;
+
+
+
+pcb_PTR mkEmptyProcQ (){
+	
+}
+
+
+int emptyProcQ (pcb_PTR tp){
+	
+}
+
+
+
+
+void freePcb (pcb_PTR p){
+	
+}
+
+
+pcb_PTR allocPcb (){
+	
+}
+
+
+void initPcbs (){
+	
+}
+
+
+void insertProcQ (pcb_PTR *tp, pcb_PTR p){
+	
+}
+
+
+
+pcb_PTR outProcQ (pcb_PTR *tp, pcb_PTR p){
+	
+}
+
+
+
+pcb_PTR removeProcQ (pcb_PTR *tp){
+	
+}
+
+
+
+pcb_PTR headProcQ (pcb_PTR tp){
+
+}
+
+
+
+int emptyChild (pcb_PTR p){
+	
+}
+
+
+void insertChild (pcb_PTR prnt, pcb_PTR p){
+	
+}
+
+
+pcb_PTR removeChild (pcb_PTR p){
+	 
+}
+
+
+
+pcb_PTR outChild (pcb_PTR p){ 
+	
+}
